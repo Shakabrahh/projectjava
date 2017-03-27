ee796fb55d668a1363a0c39e0da9ddab821126f5
Shakabrahh <dylan.viaud2@gmail.com>
2017-03-27 12:01:46 +0200

    Add files via upload

---
+++ projet javascript/js/image.js
@@ -0,0 +1,25 @@
+// $( "#lancer" ).submit(function( event ) {
+//   alert( "Handler for .submit() called." );
+//   event.preventDefault();
+//   var the_data = 'tags='+document.getElementById("searchBox").value+'&tagmode=any&format=json';
+//   alert(the_data);
+
+$(document).ready(function(){
+  $("#bouton1").click(function() {
+    var bla = $('#searchBox').val();
+    $.ajax({
+    url:'http://api.flickr.com/services/feeds/photos_public.gne',
+    type:'GET',
+    dataType:'jsonp',
+    jsonp: 'jsoncallback', // a renseigner d'après la doc du service, par défaut callback
+    data:'tags='+bla+'&tagmode=any&format=json',
+    success:function(data){
+    $.each(data.items, function(i,item){
+                $("<img/>").attr("src", item.media.m).appendTo("#images");
+                if ( i == 6 ) return false ; });
+              },
+    error: function(resultat,statut,erreur){
+    alert("erreur");},
+     });
+  });
+});
--- projet javascript/js/index.js
+++ projet javascript/js/index.js
@@ -1,18 +1,18 @@
 var searchIndex = [];
 
 $.ajax({
-type: 'GET',
-url: 'http://infoweb-ens/~jacquin-c/codePostal/commune.php',
-data: {
-             commune: null
-         },
-jsonp: 'jsoncallback',
-dataType: 'json',
-success: function (data) {
-    $.each(data, function(index, element) {
-        searchIndex.push(element.Ville);
-    });
-}
+    type: 'GET',
+    url: 'http://infoweb-ens/~jacquin-c/codePostal/commune.php',
+    data: {
+        commune: null
+    },
+    jsonp: 'jsoncallback',
+    dataType: 'json',
+    success: function(data) {
+        $.each(data, function(index, element) {
+            searchIndex.push(element.Ville);
+        });
+    }
 });
 
 var input = document.getElementById("searchBox"),
@@ -21,72 +21,86 @@ var input = document.getElementById("searchBox"),
 
 
 var search = function() {
-  inputTerms = input.value.toLowerCase();
-  results = [];
-  termsArray = inputTerms.split(' ');
-  prefix = termsArray.length === 1 ? '' : termsArray.slice(0, -1).join(' ') + ' ';
-  terms = termsArray[termsArray.length -1].toLowerCase();
+    inputTerms = input.value.toLowerCase();
+    results = [];
+    termsArray = inputTerms.split(' ');
+    prefix = termsArray.length === 1 ? '' : termsArray.slice(0, -1).join(' ') + ' ';
+    terms = termsArray[termsArray.length - 1].toLowerCase();
 
-  for (var i = 0; i < searchIndex.length; i++) {
-    var a = searchIndex[i].toLowerCase(),
-        t = a.indexOf(terms);
+    for (var i = 0; i < searchIndex.length; i++) {
+        var a = searchIndex[i].toLowerCase(),
+            t = a.indexOf(terms);
 
-    if (t > -1) {
-      results.push(a);
+        if (t > -1) {
+            results.push(a);
+        }
     }
-  }
 
-  evaluateResults();
+    evaluateResults();
 };
 
 var evaluateResults = function() {
-  if (results.length > 0 && inputTerms.length > 0 && terms.length !== 0) {
-    sortedResults = results.sort(sortResults);
-    appendResults();
-  }
-  else if (inputTerms.length > 0 && terms.length !== 0) {
-    ul.innerHTML = '<li>Whoah! <strong>'
-      + inputTerms
-      + '</strong> is not in the index. <br><small><a href="http://google.com/search?q='
-      + encodeURIComponent(inputTerms) + '">Try Google?</a></small></li>';
+    if (results.length > 2 && inputTerms.length > 2 && terms.length !== 0) {
+        sortedResults = results.sort(sortResults);
+        appendResults();
+    } else if (inputTerms.length > 0 && terms.length !== 0) {
+        ul.innerHTML = '<li>Whoah! <strong>' +
+            inputTerms +
+            '</strong> is not in the index. <br><small><a href="http://google.com/search?q=' +
+            encodeURIComponent(inputTerms) + '">Try Google?</a></small></li>';
 
-  }
-  else if (inputTerms.length !== 0 && terms.length === 0) {
-    return;
-  }
-  else {
-    clearResults();
-  }
+    } else if (inputTerms.length !== 0 && terms.length === 0) {
+        return;
+    } else {
+        clearResults();
+    }
 };
 
-var sortResults = function (a,b) {
-  if (a.indexOf(terms) < b.indexOf(terms)) return -1;
-  if (a.indexOf(terms) > b.indexOf(terms)) return 1;
-  return 0;
+var sortResults = function(a, b) {
+    if (a.indexOf(terms) < b.indexOf(terms)) return -1;
+    if (a.indexOf(terms) > b.indexOf(terms)) return 1;
+    return 0;
 }
 
-var appendResults = function () {
-  clearResults();
+var appendResults = function() {
+    clearResults();
 
-  for (var i=0; i < sortedResults.length && i < 5; i++) {
-    var li = document.createElement("li"),
-        result = prefix
-          + sortedResults[i].toLowerCase().replace(terms, '<strong>'
-          + terms
-          +'</strong>');
+    for (var i = 0; i < sortedResults.length && i < 5; i++) {
+        var li = document.createElement("li"),
+            result = prefix +
+            sortedResults[i].toLowerCase().replace(terms, '<strong>' +
+                terms +
+                '</strong>');
 
-    li.innerHTML = result;
-    ul.appendChild(li);
-  }
+        li.innerHTML = result;
+        ul.appendChild(li);
+    }
 
-  if ( ul.className !== "term-list") {
-    ul.className = "term-list";
-  }
+    if (ul.className !== "term-list") {
+        ul.className = "term-list";
+    }
+};
+
+function getEventTarget(e) {
+    e = e || window.event;
+    return e.target || e.srcElement;
+}
+
+var ul = document.getElementById('searchResults');
+ul.onclick = function(event) {
+    var target = getEventTarget(event);
+    var texte = target.innerHTML;
+    var res = texte.replace("<strong>", "");
+    var resf = res.replace("</strong>", "");
+    document.getElementById('searchBox').value = resf;
+    clearResults();
+
 };
 
 var clearResults = function() {
-  ul.className = "term-list hidden";
-  ul.innerHTML = '';
+    ul.className = "term-list hidden";
+    ul.innerHTML = '';
 };
+
 
 input.addEventListener("keyup", search, false);
