dcac56fb80d120c22ee3cf7967f557495dc0e6fa
crowlas <dracosamu@gmail.com>
2017-04-08 20:49:05 +0200

    maj
    calendar pas pris en compte par l'appli.
    click sur image à prevoir.

--- js/image.js
+++ js/image.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
     ul.innerHTML = '';
     $( "#tabs" ).show();
     $('#tabs-1').html("");
+    $('#tabs-2 table tbody').html("");
     var bla = $('#searchBox').val();
     $.ajax({
     url: "https://api.flickr.com/services/rest/",
@@ -25,19 +26,69 @@ $(document).ready(function(){
       $.each(response.photos.photo, function (index, value) {
                 console.log(value);
               var url = 'https://farm' + value.farm + '.staticflickr.com/' + value.server + '/' + value.id + '_' + value.secret + '.jpg';
-              var a = $('<a>').attr({href: url});
+              var a = $('<a>').attr({href: url, title: value.title});
               var img = $('<img>').attr({src: url});
+              var id_img = value.id;
               a.append(img);
               $("#tabs-1").append(a);
+
+              $.ajax({
+                        url: 'https://api.flickr.com/services/rest/?method=flickr.photos.getInfo',
+                        type: 'GET',
+                        dataType: 'jsonp',
+                        jsonp:   'jsoncallback',
+                        data: 'api_key=671aab1520e2cb69e08dd36a5f40213b&tagmode=any&format=json&photo_id=' + id_img,
+                        success: function(data) {
+                          var startDate = $('#date').val();
+                          var actualDate1 =(data.photo.dates.taken).split(" ")[0];
+                          var actualDate = String(actualDate1).replace('-','/');
+
+                          if ((startDate!="")||(startDate<actualDate)) {
+                            // Vue tableau --> #tabs-2
+                            t.row.add(
+                              ["<img src="+url+">", data.photo.title._content, data.photo.dates.taken ,data.photo.owner.nsid]
+                            ).draw();
+                            //console.log(data.photo.title._content);
+                            // console.log(data.photo.owner.username);
+                            // console.log(data.photo.dates.taken);
+
+
+                          }
+
+                        },
+                        error: function(resultat,statut,erreur){
+                        alert("erreur");},
+
+                        });
             });
              $('#tabs-1').justifiedGallery({
                rowHeight : 300,
                lastRow : 'nojustify',
                margins : 1
              });
+
               },
     error: function(resultat,statut,erreur){
     alert("erreur");},
      });
   });
 });
+
+// permet de demander une date à l'utilisateur
+function mydate()
+{
+  //alert("");
+document.getElementById("dt").hidden=false;
+document.getElementById("ndt").hidden=true;
+}
+function mydate1()
+{
+ d=new Date(document.getElementById("dt").value);
+dt=d.getDate();
+mn=d.getMonth();
+mn++;
+yy=d.getFullYear();
+document.getElementById("ndt").value=dt+"/"+mn+"/"+yy
+document.getElementById("ndt").hidden=false;
+document.getElementById("dt").hidden=true;
+}
